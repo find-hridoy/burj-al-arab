f89910eeadd4c4316dc1bd3c3880b661bdfc3f02
hridoy546 <mdhridoymia546@gmail.com>
2020-09-29 18:49:47 +0600

    first commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,41 @@
+import { Button, Container } from '@material-ui/core';
 import React, { useContext } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { UserContext } from '../../App';
+import { googleSignIn, initializeFrameWork } from '../../firebaseLogin';
 
 
 const Login = () => {
+    //Context use
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    //Private Route element 
+    let history = useHistory();
+    let location = useLocation();
+    let { from } = location.state || { from: { pathname: "/" } };
+    // Initialize
+    initializeFrameWork();
+    //Google SignIn function
+    const handleGoogleSignIn = () => {
+        googleSignIn()
+            .then(res => {
+                // console.log(res);
+                setLoggedInUser(res);
+                history.replace(from);
+            })
+            .catch(err => {
+                // console.log(err);
+                setLoggedInUser(err);
+            })
+    }
     return (
-        <div>
-            <h1>This is Login</h1>
-        </div>
+        <>
+            <Container>
+                <h1>This is Login</h1>
+                <Button onClick={handleGoogleSignIn} variant="contained" color="secondary">
+                    Sign In with Google
+                </Button>
+            </Container>
+        </>
     );
 };
 
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB8PK-zP2BroMGt7VDTW6ssG-5KBQqS0S0",
+    authDomain: "burj-al-arab-pro.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-pro.firebaseio.com",
+    projectId: "burj-al-arab-pro",
+    storageBucket: "burj-al-arab-pro.appspot.com",
+    messagingSenderId: "802343443703",
+    appId: "1:802343443703:web:525c06966c22288bbd47f2"
+};
+export default firebaseConfig;
